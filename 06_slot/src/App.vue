d20c03dd115e1ddda616c7956b37ed1433e9993c
orlzlL <yerincho06@gmail.com>
2024-03-25 15:07:03 +0900

    components

--- 06_slot/src/App.vue
+++ 06_slot/src/App.vue
@@ -1,11 +1,28 @@
-<script setup>
 
+<!-- 
+  컴포넌트의 lifeCycle hooks를 사용하는 방식은 2가지가 있으며
+  아래의 정리된 방법과 <script setup>으로 script를 setup 지시문으로 추가하여
+  hooks를 바로 사용할 수 있다.
+-->
+<script setup>
+  import MyComponent from './components/01_component/MyComponent.vue';
+  import SlotParent from './components/02_slotintro/SlotParent.vue';
 </script>
 
+<!-- 
+  vue는 사용자의 화면에 보여지는 부분을 내보낼 때 다음과 같이 <template> 하위에 구성하여 작성한다.
+  template은 다른 엘리먼트 요소를 감싸주는 역할을 수행하며 사용자의 화면에 렌더링될 때 제거된다.
+  (하나의 컴포넌트가 화면에 어떻게 보여질 지 컴포넌트 단위의 html이 작성된다고 볼 수 있다.)
+-->
 <template>
-  <p>Hello!</p>
+  <header>
+    <p>Hello!</p>
+    <MyComponent/>
+  </header>
+
+    <SlotParent/>
 </template>
 
-<style scoped>
+<!-- <style scoped>
 
-</style>
+</style> -->
---
+++ 06_slot/src/components/02_slotintro/SlotParent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="parentContainer">
+        <h1>Parent</h1>
+        <Child>
+            {{  slotData }}
+        </Child>
+    </div>
+</template>
+
+<!-- 
+    slot:
+    슬롯(slot)은 Vue 컴포넌트에서 컨텐츠를 전달하는 매커니즘으로 부모 컴포넌트에서 자식 컴포넌트로
+    데이터나 구조를 전달하는데 사용된다.
+    (주로 구조(부모에서 만든 컴포넌트 요소)를 전달하는 데 목적이 있음, 데이터는 prop으로..)
+-->
+<script setup>
+    import Child from "./SlotChild.vue";
+    import {ref} from 'vue';
+
+    const slotData = ref('hello Data');
+</script>
+
+<style scoped>
+    .parentContainer {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        border: 1px solid black;
+    }
+</style>
